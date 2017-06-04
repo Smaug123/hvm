cf8f2f0630af1678daeb687ab936a6c70233fdf4
Smaug123 <patrick@patrickstevens.co.uk>
2017-06-04 08:24:44 +0100

    Initial Commit

---
+++ hvm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hvm
+//
+//  Created by Patrick Stevens on 04/06/2017.
+//  Copyright © 2017 Patrick Stevens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
